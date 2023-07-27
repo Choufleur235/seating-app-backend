b9ab4dbd35f656bc1f318a46b87eab6c245f7936
Choufleur235 <bidaultchloe@gmail.com>
2023-07-27 07:51:18 +0100

    Added cors

--- server.js
+++ server.js
@@ -3,15 +3,22 @@ require("dotenv").config()
 const express = require("express");
 const mongoose = require("mongoose");
 const tableRoutes = require("./routes/tables");
+const cors=require("cors");
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
 
 
 const app = express();
 
 
 //middleware
-    //only for the post ?
 app.use(express.json());
 app.use(express.static("./my-app/public"));
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
     //
 
